28197aa17aac9f7918e8ef7d45313786e45cea67
tompiasek <tompiaseczny2003@gmail.com>
2024-01-16 17:57:16 +0100

    v2.0 - Full demosaic and convolution implementation
    - Mosaic images (CFA simulation)
    - Demosaicing images with every technique
    - Comparing metrics (MSE, PSNR, SSIM)
    - No Fuji X-Trans implementation yet
    - Convolution on gray and RGB (1 or 3 channels)
    - Edge detection with:
      - Sobel X and Y axis
      - Full Sobel, Sobel-Feldman
      - Laplace
    - Image blurring
    - Image sharpening

--- main.py
+++ main.py
@@ -100,33 +100,37 @@ if __name__ == '__main__':
                   [255, 255, 255, 255, 128, 255, 255, 255, 255],
                   [255, 255, 255, 255, 255, 255, 255, 255, 255]]
 
-    original_img = image
-    image_interp = simulate_cfa_3d(image)  # Simulate CFA
+    # original_img = image
+    # image_interp = simulate_cfa_3d(image)  # Simulate CFA
 
-    image_interp = demosaic_bayer_interp(image_interp)  # Demosaic image
+    # image_interp = demosaic_bayer_interp(image_interp)  # Demosaic image
+    image_interp = sk.io.imread("img/saved/Demosaicing/interp_demosaic_min.jpg")
 
     """ DEMOSAICING - CONVOLUTION """
 
-    image_conv = simulate_cfa_3d(original_img)  # Simulate CFA
+    image_conv = simulate_cfa_3d(image)  # Simulate CFA
 
     # Demosaicing kernel (e.g., bi-linear)
     kernel = np.array([[0.25, 0.5, 0.25],
                        [0.5, 1, 0.5],
-                       [0.25, 0.5, 0.25]])
+                       [0.25, 0.5, 0.25]], dtype='float64')
 
     kernel_d = np.array([[1, 1], [1, 1]]).astype('float64')
-
-    # Normalize the kernel
     kernel /= np.sum(kernel)
 
     # Perform demosaicing
-    final_conv_img = demosaic_bayer_conv(image_conv, kernel)
-    final_conv_img_d = demosaic_bayer_conv(image_conv, kernel_d)
+    # final_conv_img = demosaic_bayer_conv(image_conv, kernel)
+    # final_conv_img_d = demosaic_bayer_conv(image_conv, kernel_d)
 
-    """ COMPARE DIFFERENT DEMOSAICING TECHNIQUES """
+    # Read demosaiced images from files to save time
+    final_conv_img = sk.io.imread("img/saved/Demosaicing/conv_demosaic_bilinear_min.jpg")
+    final_conv_img_d = sk.io.imread("img/saved/Demosaicing/conv_demosaic_midkernel_min.jpg")
+
+    """ COMPARE DIFFERENT DEMOSAICING TECHNIQUES
 
     full_res_img = sk.io.imread("img/test_min.jpg")
     # full_res_img = sk.transform.resize(full_res_img, (len(image_conv), len(image_conv[0])))
+
 
     print("Interpolation MSE: " + str(np.round(sk.metrics.mean_squared_error(full_res_img, image_interp), 4)) + "\n" +
           "Convolution (bi-linear) MSE: " + str(np.round(sk.metrics.mean_squared_error(full_res_img, final_conv_img), 4)) + "\n" +
@@ -143,16 +147,20 @@ if __name__ == '__main__':
     print("Interpolation SSIM: " + str(interp_compare[1]) + "\n" +
           "Convolution (bi-linear) SSIM: " + str(conv_compare[1]) + "\n" +
           "Convolution (averaging) SSIM: " + str(conv_compare_d[1]))
+    
+    """
 
-    """ COMPARE INTERPOLATION AND CONVOLUTION """
-    # sim_interp_img = simulate_cfa_3d(image_interp)  # Simulate CFA
-    # sim_conv_img = simulate_cfa_3d(final_conv_img)  # Simulate CFA
-    #
-    # interp_mse = sk.metrics.mean_squared_error(image_conv, sim_interp_img)
-    # conv_mse = sk.metrics.mean_squared_error(image_conv, sim_conv_img)
-    #
-    # print("Interpolation MSE: " + str(interp_mse))
-    # print("Convolution MSE: " + str(conv_mse))
+    """ COMPARE INTERPOLATION AND CONVOLUTION 
+    sim_interp_img = simulate_cfa_3d(image_interp)  # Simulate CFA
+    sim_conv_img = simulate_cfa_3d(final_conv_img)  # Simulate CFA
+
+    interp_mse = sk.metrics.mean_squared_error(image_conv, sim_interp_img)
+    conv_mse = sk.metrics.mean_squared_error(image_conv, sim_conv_img)
+
+    print("Interpolation MSE: " + str(interp_mse))
+    print("Convolution MSE: " + str(conv_mse))
+
+    """
 
     """ PLOTTING """
 
@@ -164,18 +172,19 @@ if __name__ == '__main__':
     cm_g = LinearSegmentedColormap.from_list('green', greens, N=20)
     cm_b = LinearSegmentedColormap.from_list('blue', blues, N=20)
 
-    # Plotting the channels
-    _, ax = plt.subplots(2, 2, figsize=(14, 10), gridspec_kw={'left': 0.03, 'right': 0.97, 'top': 0.96, 'bottom': 0.04, 'wspace': 0.11, 'hspace': 0.2})
 
-    ax[0][0].imshow(image_interp)  # Demosaiced image
-    ax[0][0].set_title('Demosaiced image')
-    ax[0][1].imshow(image_interp[:, :, 0], cmap=cm_r)  # Red channel
-    ax[0][1].set_title('Red channel')
-    ax[1][0].imshow(image_interp[:, :, 1], cmap=cm_g)  # Green channel
-    ax[1][0].set_title('Green channel')
-    ax[1][1].imshow(image_interp[:, :, 2], cmap=cm_b)  # Blue channel
-    ax[1][1].set_title('Blue channel')
-
+    # # Plotting the channels
+    # _, ax = plt.subplots(2, 2, figsize=(14, 10), gridspec_kw={'left': 0.03, 'right': 0.97, 'top': 0.96, 'bottom': 0.04, 'wspace': 0.11, 'hspace': 0.2})
+    #
+    # ax[0][0].imshow(image_interp)  # Demosaiced image
+    # ax[0][0].set_title('Demosaiced image')
+    # ax[0][1].imshow(image_interp[:, :, 0], cmap=cm_r)  # Red channel
+    # ax[0][1].set_title('Red channel')
+    # ax[1][0].imshow(image_interp[:, :, 1], cmap=cm_g)  # Green channel
+    # ax[1][0].set_title('Green channel')
+    # ax[1][1].imshow(image_interp[:, :, 2], cmap=cm_b)  # Blue channel
+    # ax[1][1].set_title('Blue channel')
+    """
     # Plotting comparison between original and interpolation
     _, ax2 = plt.subplots(1, 2, figsize=(12, 6), gridspec_kw={'left': 0.03, 'right': 0.97, 'wspace': 0.11})
 
@@ -191,6 +200,7 @@ if __name__ == '__main__':
     ax3[0].set_title('Original Bayer Image')
     ax3[1].imshow(final_conv_img)
     ax3[1].set_title('Demosaiced Image (Convolution)')
+    """
 
     # Plotting comparison between convolution and interpolation
     _, ax4 = plt.subplots(1, 2, figsize=(12, 6), gridspec_kw={'left': 0.03, 'right': 0.97, 'wspace': 0.11})
@@ -205,21 +215,90 @@ if __name__ == '__main__':
     # ax4[1].imshow(final_conv_img_d)
     # ax4[1].set_title('Demosaiced Image (averaging)')
 
+    A = np.array([[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                  [0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0],
+                  [0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0],
+                  [0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0],
+                  [0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0],
+                  [0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0],
+                  [0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0],
+                  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]])
+
+    """ CONVOLUTION KERNELS """
     kernel_edge = np.array([[0, 1, 0],
                             [1, -4, 1],
                             [0, 1, 0]])
-    kernel_edge_s = np.array([[1, 2, 1],
-                             [1, 4, 1],
-                             [1, 2, 1]])
+    kernel_sobel_x = np.array([[1, 0, -1],
+                               [2, 0, -2],
+                               [1, 0, -1]], dtype='float64')
+    kernel_sobel_y = np.array([[1, 2, 1],
+                               [0, 0, 0],
+                               [-1, -2, -1]], dtype='float64')
+    kernel_mid = (np.array([[1, 1, 1],
+                           [1, 1, 1],
+                           [1, 1, 1]]) / 9).astype('float64')
+    kernel_gauss = np.array([[1, 2, 1],
+                           [2, 4, 2],
+                           [1, 2, 1]], dtype='float64')
+    kernel_gauss /= np.sum(kernel_gauss)
+    kernel_gauss_5x5 = np.array([[1, 4, 6, 4, 1],
+                                [4, 16, 24, 16, 4],
+                                [6, 24, 36, 24, 6],
+                                [4, 16, 24, 16, 4],
+                                [1, 4, 6, 4, 1]], dtype='float64')
+    kernel_gauss_5x5 /= np.sum(kernel_gauss_5x5)
+    kernel_sharp = np.array([[0, -1, 0],
+                            [-1, 5, -1],
+                            [0, -1, 0]])
 
-    ax4[0].imshow(convolve2d(final_conv_img, kernel_edge))
-    ax4[0].set_title('Edge detection (bi-linear)')
-    ax4[1].imshow(convolve2d(final_conv_img, kernel_edge_s))
-    ax4[1].set_title('Edge detection (averaging)')
+    nyc_test_img = sk.io.imread("img/edge_test_nyc.png")[:, :, :3]
+
+    grayscale_conv_int = np.array(sk.color.rgb2gray(nyc_test_img) * 255, dtype='uint8')
+    grayscale_conv_float = sk.color.rgb2gray(nyc_test_img)
+
+    # Sobel edge detection (X and Y)
+    sobel_x_img = np.asarray(convolve2d(np.asarray(grayscale_conv_int).astype('float64') / 255, kernel_sobel_x)).astype(np.float64)
+    sobel_y_img = np.asarray(convolve2d(np.asarray(grayscale_conv_int).astype('float64') / 255, kernel_sobel_y)).astype(np.float64)
+
+    # Sobel filter (detector (X + Y))
+    fake_sobel = sobel_x_img + sobel_y_img
+    fake_sobel /= np.max(fake_sobel)
+    fake_sobel = np.clip(fake_sobel, 0, 1)
+
+    # Sobel-Feldman filter
+    sobel_edge_img = np.asarray(np.sqrt(sobel_x_img * sobel_x_img + sobel_y_img * sobel_y_img)).astype('float64')
+    if np.max(sobel_edge_img) > 1:
+        sobel_edge_img /= np.max(sobel_edge_img)
+
+
+    # not_clipped = convolve2d(nyc_test_img, kernel_edge)
+    # clipped = np.clip(not_clipped, 0, 255)
+    # clipped_uint8 = clipped.astype(np.uint8)
+
+    # ax4[0].imshow(sobel_edge_img, cmap='gray')
+    # ax4[0].set_title('Edge detection (Sobel filter)')
+    # ax4[1].imshow(np.clip(convolve2d(grayscale_conv_float, kernel_edge)*3, 0, 1), cmap='gray')
+    # ax4[1].set_title('Edge detection (Laplace filter (amplified x3)')
+
+    # DISPLAY BASIC EDGE DETECTION (LAPLACE FILTER) WITH CHANNELS
+    # _, ax5 = plt.subplots(2, 2, figsize=(14, 10), gridspec_kw={'left': 0.03, 'right': 0.97, 'top': 0.96, 'bottom': 0.04, 'wspace': 0.11, 'hspace': 0.2})
+    #
+    # ax5[0][0].imshow(not_clipped)
+    # ax5[0][0].set_title('Edge detection (bi-linear)')
+    # ax5[0][1].imshow(not_clipped[:, :, 0], cmap=cm_r)
+    # ax5[0][1].set_title('Red channel')
+    # ax5[1][0].imshow(not_clipped[:, :, 1], cmap=cm_g)
+    # ax5[1][0].set_title('Green channel')
+    # ax5[1][1].imshow(not_clipped[:, :, 2], cmap=cm_b)
+    # ax5[1][1].set_title('Blue channel')
 
     # Save images
-    # sk.io.imsave("img/saved/interp_demosaic_min.jpg", np.ndarray.astype(image_interp, np.uint8))
-    # sk.io.imsave("img/saved/conv_demosaic_bilinear_min.jpg", np.ndarray.astype(final_conv_img, np.uint8))
-    # sk.io.imsave("img/saved/conv_demosaic_midkernel_min.jpg", np.ndarray.astype(final_conv_img_d, np.uint8))
+    # sk.io.imsave("img/saved/interp_demosaic_min_defects.jpg", np.ndarray.astype(image_interp[400:630, 65:500, :], np.uint8))
+    # sk.io.imsave("img/saved/conv_demosaic_bilinear_min_defects.jpg", np.ndarray.astype(final_conv_img[400:630, 65:500, :], np.uint8))
+    # sk.io.imsave("img/saved/conv_demosaic_midkernel_min_defects.jpg", np.ndarray.astype(final_conv_img_d[400:630, 65:500, :], np.uint8))
 
     plt.show()
--- src/convolution.py
+++ src/convolution.py
@@ -7,6 +7,7 @@ def convolve2d(img, kernel, pad_val=0):
     result = np.zeros_like(img)
     krn_height, krn_width = kernel.shape
     pad_height, pad_width = krn_height // 2, krn_width // 2
+
     if len(img.shape) == 2:
         padded_image = np.pad(img, ((pad_height, pad_height), (pad_width, pad_width)), mode='constant', constant_values=pad_val)
 
@@ -15,19 +16,11 @@ def convolve2d(img, kernel, pad_val=0):
                 result[i, j] = np.sum(padded_image[i:i+krn_height, j:j+krn_width] * kernel)
 
     elif len(img.shape) == 3:
-        padded_red = np.pad(img[:, :, 0], ((pad_height, pad_height), (pad_width, pad_width)), mode='constant', constant_values=pad_val)
-        padded_green = np.pad(img[:, :, 1], ((pad_height, pad_height), (pad_width, pad_width)), mode='constant', constant_values=pad_val)
-        padded_blue = np.pad(img[:, :, 2], ((pad_height, pad_height), (pad_width, pad_width)), mode='constant', constant_values=pad_val)
-        padded_image = np.dstack([padded_red, padded_green, padded_blue])
         for ch in range(result.shape[2]):
+            padded_channel = np.pad(img[:, :, ch], ((pad_height, pad_height), (pad_width, pad_width)), mode='constant', constant_values=pad_val)
             for i in range(result.shape[0]):
                 for j in range(result.shape[1]):
-                    if np.sum(kernel) != 0:
-                        result[i, j, ch] = np.sum(padded_image[i:i+krn_height, j:j+krn_width, ch] * kernel) / np.sum(kernel)
-                    else:
-                        result[i, j, ch] = np.sum(padded_image[i:i+krn_height, j:j+krn_width, ch] * kernel)
-                        print("The sum of kernel: " + str(np.sum(kernel)) + "\n" +
-                              "Channel: " + str(ch) + "\n")
+                    result[i, j, ch] = np.clip(np.sum(padded_channel[i:i+krn_height, j:j+krn_width] * kernel), 0, 255)
 
     else:
         print("Err: Wrong image format!" + "\n" +
@@ -45,6 +38,11 @@ def normalize_image(img):
         if max_val > 1:
             return ((img - min_val) / (max_val - min_val) * 255).astype(np.uint8)
         else:
+            print("Normalization for float values..." + "\n" +
+                  "Min: " + str(min_val) + "\n" +
+                  "Max: " + str(max_val) + "\n")
+            print("Normalization max: " + str(np.max((img - min_val) / (max_val - min_val))) + "\n" +
+                  "Normalization min: " + str(np.min((img - min_val) / (max_val - min_val))) + "\n")
             return ((img - min_val) / (max_val - min_val)).astype(np.float32)
     elif len(img.shape) == 3:
         for ch in range(img.shape[2]):
